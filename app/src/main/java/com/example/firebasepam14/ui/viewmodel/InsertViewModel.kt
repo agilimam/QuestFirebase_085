5837a2f987981a27224744791176ec581755054a
agilimam <agilimamalhafizi04@gmail.com>
2025-01-12 19:32:39 +0700

    Menambahkan data class untuk menyimpan data input form Mahasiswa.

---
+++ app/src/main/java/com/example/firebasepam14/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.firebasepam14.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasepam14.model.Mahasiswa
+import com.example.firebasepam14.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+//data class variabel yang menyimpan data input from
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+//menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
