4dcd48a64192841a433162d37d08d0c59420b21f
agilimam <agilimamalhafizi04@gmail.com>
2025-01-15 14:07:33 +0700

    memanggil DestinasiDetail

--- app/src/main/java/com/example/firebasepam14/ui/navigate/PengelolaHalaman.kt
+++ app/src/main/java/com/example/firebasepam14/ui/navigate/PengelolaHalaman.kt
@@ -3,9 +3,13 @@ package com.example.firebasepam14.ui.navigate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.firebasepam14.ui.view.DetailScreen
 import com.example.firebasepam14.ui.view.HomeScreen
 import com.example.firebasepam14.ui.view.InsertMhsView
 
@@ -20,21 +24,48 @@ fun PengelolaHalaman(
         startDestination = DestinasiHome.route,
         modifier = Modifier
     ) {
-        composable(DestinasiHome.route){
+        composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = {
                     navController.navigate(DestinasiInsert.route)
                 },
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                }
+
             )
         }
         composable(DestinasiInsert.route) {
             InsertMhsView(
-                onBack = {navController.popBackStack()},
+                onBack = { navController.popBackStack() },
                 onNavigate = {
                     navController.navigate(DestinasiHome.route)
                 }
             )
         }
+        composable(
+            DestinasiDetail.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let {
+                DetailScreen(
+                    navigateBack = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    navigateToItemUpdate = {
+                        navController.navigate("")
+                    },
+                )
+            }
+        }
     }
-
 }
